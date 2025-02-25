e9640f74d3bebeb1d55037e60e176f41db859e1e
Sambhav <sambhav12wakhariya@gmail.com>
2025-02-25 23:17:10 +0530

    Checked In : Week 15 Day 2 Routes File Added In Backend

--- Week 15/Week 15.2/Backend/src/routes/index.routes.js
+++ Week 15/Week 15.2/Backend/src/routes/index.routes.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const upload = require('../utils/multer');
+
 const userMiddleware = require('../middlewares/user.middleware');
 
 const indexController = require('../controller/index.controller');
 
 router.get('/', indexController.indexController);
 router.get('/feed', userMiddleware.authUser, indexController.feedController);
+router.post('/upload', upload.single("file"), indexController.handleUpload);
 
 
 module.exports = router;
