4a213dec6f98e96f514603b773aa698951b7ef19
Sambhav <sambhav12wakhariya@gmail.com>
2025-04-15 23:58:12 +0530

    Checked In : JavaScript Interview Prep Data Types

---
+++ Interview Preparation/JavaScript/Data Types/index.js
@@ -0,0 +1,21 @@
+let str = "String";
+let num = 45;
+let bigInt = 1234567890123456789012345678901234567890n;
+let bool = true;
+let undef = undefined;
+let nul = null;
+let sym = Symbol("sym");
+
+let obj = {
+  name: "Sambhav",
+  age: 20,
+  isStudent: true,
+};
+
+let arr = [1, 2, 3, 4, 5];
+
+function name(params) {
+    console.log("this is a function");    
+}
+
+console.log(typeof arr);
